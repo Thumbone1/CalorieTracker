9b7c3c2bf3e4748fc17cab0fde26e69483384669
Judge Gambill <judgegambill@gmail.com>
2018-05-26 14:03:04 +0200

    Cleaned up header and author

--- src/calorietracker/CalorieTracker.java
+++ src/calorietracker/CalorieTracker.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package calorietracker;
 
 import java.io.BufferedReader;
@@ -16,7 +11,7 @@ import java.io.PrintWriter;
 
 /**
  *
- * @author judgeandsiri
+ * @author Thumbone1
  */
 public class CalorieTracker {
     
